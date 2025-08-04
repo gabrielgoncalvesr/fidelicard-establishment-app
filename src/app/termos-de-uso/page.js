6623a6be449a182097e28c3fa30398ee1879e8f5
Gabriel Gonçalves <gabriel.goncalves@veripag.com.br>
2025-08-04 11:49:41 -0300

    fix

--- src/app/termos-de-uso/page.js
+++ src/app/termos-de-uso/page.js
@@ -7,7 +7,7 @@ export default function TermosDeUsoPage() {
       <p>Ao acessar e usar nosso aplicativo, você aceita e concorda em ficar vinculado aos termos e disposições deste acordo. Além disso, ao usar estes serviços específicos, você estará sujeito a quaisquer diretrizes ou regras aplicáveis.</p>
       
       <h2>2. Descrição do Serviço</h2>
-      <p>Nosso aplicativo fornece uma plataforma de fidelidade digital. Você entende e concorda que o Serviço é fornecido "COMO ESTÁ" e que não assumimos responsabilidade pela pontualidade, exclusão, falha na entrega ou falha no armazenamento de quaisquer comunicações ou configurações de personalização do usuário.</p>
+      <p>Nosso aplicativo fornece uma plataforma de fidelidade digital. Você entende e concorda que o Serviço é fornecido &quot;COMO ESTÁ&quot; e que não assumimos responsabilidade pela pontualidade, exclusão, falha na entrega ou falha no armazenamento de quaisquer comunicações ou configurações de personalização do usuário.</p>
 
       <h2>3. Modificações nos Termos</h2>
       <p>Reservamo-nos o direito de alterar estes termos de tempos em tempos, a nosso exclusivo critério. Portanto, você deve revisar esta página periodicamente.</p>
